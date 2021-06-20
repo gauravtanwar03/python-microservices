292a3afdff38d0d66034139d8a27d1952cec7257
Gaurav Kumar <gauravtanwar03@gmail.com>
2021-06-20 10:39:27 +0530

    Added fake movie db

--- movie_service/app/main.py
+++ movie_service/app/main.py
@@ -1,8 +1,26 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import List
 
 app = FastAPI()
 
+fake_movie_db = [
+    {
+        "name": "Avengers: End Game",
+        "plot": "Avengers tries to undo the snap done by Thanos, their greatest enemy!",
+        "genres": ["Action", "Superhero", "SciFi", "Adventure"],
+        "casts": ["Robert Downey Junior"],
+    }
+]
 
-@app.get("/")
+
+class Movie(BaseModel):
+    name: str
+    plot: str
+    genres: List[str]
+    casts: List[str]
+
+
+@app.get("/", response_model=List[Movie])
 async def index():
-    return {"First": "App"}
+    return fake_movie_db
